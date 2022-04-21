0b29b8c3e2f4f202bf6bc8e40444b6c4f7d9ccd0
Berty <38094933+Cariloha@users.noreply.github.com>
2022-04-20 23:49:21 -0500

    wip

--- animation/js/script.js
+++ animation/js/script.js
@@ -2,12 +2,32 @@
 //Scene
 const scene = new THREE.Scene();
 
+
+
 //Red cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color: '#ff0000'});
 const mesh = new THREE.Mesh(geometry, material);
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
 
 scene.add(mesh);
+
+//Scale
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.x = 0.5;
+
+//Rotate has two ways of working: rotation or quaternion
+// mesh.rotation.reorder('YXZ'); //This goes before changing the rotation
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
+
+
+//Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 //Sizes 
 const sizes = {
@@ -17,8 +37,13 @@ const sizes = {
 
 //Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) //field of view, aspect ratio (width / height), 
+// camera.position.x = 1;
+// camera.position.y = 1;
 camera.position.z = 3;
+
 scene.add(camera);
+
+camera.lookAt(mesh.position);
 
 //Renderer
 const canvas = document.querySelector('.webgl');
@@ -27,4 +52,33 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height);
 
-renderer.render(scene, camera);
+// renderer.render(scene, camera);
+
+// Time
+
+let time = Date.now();
+
+//Animations
+
+const tick = () => 
+{
+
+    //Time
+    const currentTime = Date.now();
+    const deltaTime = currentTime - time;
+    time = currentTime;
+
+    //To move in circle:
+    camera.position.y = Math.sin(0.001 * time);
+    camera.position.x = Math.cos(0.001 * time);
+    camera.lookAt(mesh.position);
+
+    // mesh.rotation.y += 0.01 * deltaTime;
+
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick)
+
+    
+}
+
+tick();
---
+++ gruop.js
@@ -0,0 +1,25 @@
+//Objects
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff})
+)
+cube3.position.x = 2;
+group.add(cube3);
